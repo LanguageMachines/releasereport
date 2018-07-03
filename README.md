dbbfc33627dacf7d04b5377fa68f79349f060e4a
Maarten van Gompel <proycon@anaproy.nl>
2018-07-03 23:05:15 +0200

    adding more projects so I can also use this for non-CLARIAH purposes, added a verbose output option that adds descriptions and refactored a bit

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ The reports are in ``reports/``
 ## Technical Details
 
 * First set the ``GITHUB_USER`` and ``GITHUB_PASS`` environment variables (otherwise you quickly hit the Github API limit)
-* Run ``releasereport.py`` script, it takes a begin data and (optionally) and end date as arguments, both in ``YYYY-MM-DD`` format. Output is to stdout.
+* Run ``releasereport.py`` script, it takes a begin date with ``--begin`` and (optionally) an end date with ``--end``, both in ``YYYY-MM-DD`` format. Output is to stdout.
 * Markdown to PDF conversion is done as follows with ``pandoc``: ``pandoc -s -f gfm -H ../header.sty -o 2018-04-03-to-2018-06-21.pdf 2018-04-03-to-2018-06-21.md``
 
 
--- releasereport.py
+++ releasereport.py
@@ -4,55 +4,56 @@ import sys
 import requests
 import datetime
 import os
+from collections import defaultdict
 from requests.auth import HTTPBasicAuth
 
+import argparse
+parser = argparse.ArgumentParser(description="Report generator", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--all', help="Add non-CLARIN projects as well",  action='store_true')
+parser.add_argument('--verbose', help="List all software, even if there are no releases, and adds descriptions",  action='store_true')
+parser.add_argument('--begin', type=str,help="Begin date (YYYY-MM-DD)", action='store',default="",required=True)
+parser.add_argument('--end', type=str,help="End date (YYYY-MM-DD)", action='store',default=datetime.datetime.now().strftime("%Y-%m-%d"),required=False)
+parser.add_argument('positional', nargs='+', help='bar help')
+args = parser.parse_args()
+#args.storeconst, args.dataset, args.num, args.bar
 
-try:
-    begindate = sys.argv[1]
-except:
-    print("Enter a begin date (YYYY-MM-DD!)",file=sys.stderr)
-    sys.exit(2)
-try:
-    enddate = sys.argv[2]
-except:
-    enddate = datetime.datetime.now().strftime("%Y-%m-%d")
 
 username = os.environ['GITHUB_USER']
 pw = os.environ['GITHUB_PASS']
 
 sources = [
-    ("CLAM","CLARIAH T21 [D2.8 (software); D2.9 (doc)]", [
+    ("CLAM","CLARIAH WP3 T21 [D2.8 (software); D2.9 (doc)]", [
         ("proycon", "clam"),
         ("proycon", "clamservices"),
     ]),
-    ("FoLiA","CLARIAH T71 [D2.5 (libs); D2.6 (docs); D2.7 (tools)]", [
+    ("FoLiA","CLARIAH WP3 T71 [D2.5 (libs); D2.6 (docs); D2.7 (tools)]", [
         ("proycon", "folia"),
         ("proycon", "pynlpl"),
         ("LanguageMachines", "libfolia"),
         ("LanguageMachines", "foliautils"),
         ("cltl", "NAFFoLiAPy"),
     ]),
-    ("FLAT","CLARIAH T24 [D2.10 (software); D2.11 (docs)]", [
+    ("FLAT","CLARIAH WP3 T24 [D2.10 (software); D2.11 (docs)]", [
         ("proycon", "flat"),
         ("proycon", "foliadocserve"),
     ]),
-    ("LaMachine","CLARIAH: LaMachine v2 plan in scope of CLARIAH WP3 VRE", [
+    ("LaMachine","CLARIAH WP3: LaMachine v2 plan in scope of CLARIAH WP3 VRE", [
         ("proycon", "LaMachine"),
         ("proycon", "labirinto"),
         ("proycon", "codemetapy"),
     ]),
-    ("Ucto","CLARIAH T55 [D1.1 (software), D1.2 (docs)]", [
+    ("Ucto","CLARIAH WP3 T55 [D1.1 (software), D1.2 (docs)]", [
         ("LanguageMachines", "ucto"),
         ("LanguageMachines", "uctodata"),
         ("proycon", "python-ucto"),
     ]),
-    ("Frog","CLARIAH T22 [D2.1 (software); D2.2 (doc)]; T23 [D2.3 (froggen software), D2.4 (froggen docs)]", [
+    ("Frog","CLARIAH WP3 T22 [D2.1 (software); D2.2 (doc)]; T23 [D2.3 (froggen software), D2.4 (froggen docs)]", [
         ("LanguageMachines", "frog"),
         ("LanguageMachines", "frogdata"),
         ("proycon", "python-frog"),
         ("LanguageMachines", "toad"),
     ]),
-    ("PICCL & TICCL","CLARIAH T26 [D1.3 (PICCL software); T1.4 (PICCL docs)]", [
+    ("PICCL & TICCL","CLARIAH WP3 T26 [D1.3 (PICCL software); T1.4 (PICCL docs)]", [
         ("LanguageMachines", "PICCL"),
         ("LanguageMachines", "ticcltools"),
     ]),
@@ -65,10 +66,68 @@ sources = [
     ]),
     ("Miscellaneous","Dependants of others", [
         ("LanguageMachines", "ticcutils"),
-    ])
+    ]),
+    ("Software Quality Guidelines","CLARIAH WP2 Task 54.100", [
+        ("CLARIAH", "software-quality-guidelines"),
+    ]),
 ]
 
-#map github logins to full names
+if args.all:
+    sources += [
+        ("Gecco & Valkuil", "Implicit Linguisitics, Revisely", [
+            ("proycon","gecco"),
+            ("proycon","valkuil-gecco"),
+            ("proycon","valkuil"),
+        ]),
+        ("Spreek2Schrijf", "Tweede Kamer der Staten Generaal", [
+           ("proycon", "spreek2schrijf"),
+        ]),
+        ("Colibri", "Colibri PhD project", [
+           ("proycon", "colibri-core"),
+           ("proycon", "colibri-mt"),
+           ("proycon", "colibrita"),
+        ]),
+        ("Colloquery", "Van Dale", [
+            ("proycon","colloquery"),
+        ]),
+        ("T-Scan", "Utrecht University", [
+            ("proycon","tscan"),
+        ]),
+        ("Oersetter", "Fryske Akademy", [
+            ("proycon","oersetter-webservice"),
+            ("proycon","oersetter-models"),
+        ]),
+        ("BabelEnte", "TraMOOC", [
+            ("proycon","babelente"),
+            ("proycon","babelpy"),
+        ]),
+        ("Deprecated projects", "(unfunded)", [
+            ("LanguageMachine", "LuigiNLP"),
+        ]),
+        ("Shared-task software", "(unfunded)", [
+            ("LanguageMachines" ,"CLIN28_ST_spelling_correction"),
+            ("proycon" ,"anavec"),
+        ]),
+        ("Various support software", "(mostly unfunded)", [
+            ("proycon", "lamastats"),
+            ("proycon", "parseme-support"),
+            ("proycon", "beeldbankvertaler"),
+        ]),
+    ]
+
+
+users = set()
+for _,_, repos in sorted(sources):
+    for user, repo in repos:
+        users.add(user)
+
+#Get a list of all repositories for the users
+user_repos = defaultdict(dict)
+for user in users:
+    for repo in requests.get(url=f"https://api.github.com/users/{user}/repos", auth=HTTPBasicAuth(username, pw), headers=headers).json():
+        user_repos[user][repo['name'].lower()] = repo
+
+#map github logins to verbose names
 names = {
     'proycon': "Maarten van Gompel",
     'kosloot': "Ko van der Sloot",
@@ -88,8 +147,8 @@ def cleanbody(s):
 
 
 print("# Software Release and Progress Report\n")
-print("Start date: ", begindate)
-print("End date: ", enddate)
+print("Start date: ", args.begin)
+print("End date: ", args.end)
 print()
 print("## Short Summary\n")
 print("(TODO: add manually)")
@@ -102,22 +161,37 @@ headers = {
 for group, references, repos in sorted(sources):
     print(f"## {group}")
     print()
-    print("*Official task and deliverable references:* ", references)
+    if args.all:
+        print("*Project/Task/Funder:* ", references)
+    else:
+        print("*Official task and deliverable references:* ", references)
     print()
     found = False
     for user, repo in sorted(repos, key= lambda x: x[1]):
         releases = requests.get(url=f"https://api.github.com/repos/{user}/{repo}/releases", auth=HTTPBasicAuth(username, pw), headers=headers).json()
+        if args.verbose:
+            found = False
+            print("### " + repo)
+            print()
+            print("*Description:* ", user_repos[user][repo.lower()]['description'])
+            print()
         for release in releases:
-            if release['published_at'][:10] >= begindate:
+            if release['published_at'][:10] >= args.begin:
                 found = True
-                print("### " + repo + " " + release['tag_name'])
+                if args.verbose:
+                    print("#### " + repo + " " + release['tag_name'])
+                else:
+                    print("### " + repo + " " + release['tag_name'])
                 print()
                 print(cleanbody(release['body']))
                 print()
                 print(f"*(Released by " + names[release['author']['login']] + " on " + release['published_at'][:10] + ")*\n"  + f"https://github.com/{user}/{repo}/releases/tag/" + release['tag_name'])
                 print()
-    if not found:
-        print(f"*(no releases since {begindate})*")
+        if not found and args.verbose:
+            print(f"*(no releases since {args.begin})*")
+            print()
+    if not found and not args.verbose:
+        print(f"*(no releases since {args.begin})*")
         print()
     print()
 
