2661f0fcbcf71595ec632068c1819f264431bff6
Maarten van Gompel <proycon@anaproy.nl>
2020-10-13 15:01:51 +0200

    update releasereport script, adding deliverable IDs and linkable task IDs

--- releasereport.py
+++ releasereport.py
@@ -21,106 +21,105 @@ username = os.environ['GITHUB_USER']
 pw = os.environ['GITHUB_PASS']
 
 sources = [
-    ("CLAM","CLARIAH-PLUS WP3 T142; CLARIAH-CORE WP3 T21 [D2.8 (software); D2.9 (doc)]", [
-        ("proycon", "clam"),
-        ("proycon", "clamservices"),
+    ("CLAM","CLARIAH-PLUS WP3 T142", "https://github.com/LanguageMachines/clariah-plus-tasks/raw/master/di.huc/CLAM.T142.pdf", [
+        ("proycon", "clam", "T142D1" ),
+        ("proycon", "clamservices",""),
     ]),
-    ("FoLiA","CLARIAH-PLUS WP3 T108; CLARIAH-CORE WP3 T71 [D2.5 (libs); D2.6 (docs); D2.7 (tools)]", [
-        ("proycon", "folia"),
-        ("proycon", "foliapy"),
-        ("proycon", "foliatools"),
-        ("proycon", "folia-rust"),
-        ("proycon", "pynlpl"),
-        ("LanguageMachines", "libfolia"),
-        ("LanguageMachines", "foliautils"),
-        ("cltl", "NAFFoLiAPy"),
+    ("FoLiA","CLARIAH-PLUS WP3 T108", "https://github.com/LanguageMachines/clariah-plus-tasks/raw/master/di.huc/FoLiA.T108.pdf", [
+        ("proycon", "folia", "T108D1"),
+        ("proycon", "foliapy", "T108D3"),
+        ("LanguageMachines", "libfolia", "T108D4"),
+        ("proycon", "folia-rust", "T108D5"),
+        ("proycon", "foliatools", "T108D6"),
+        ("LanguageMachines", "foliautils", "T108D7"),
+        ("proycon", "piereling", "T108D9"),
+        ("cltl", "NAFFoLiAPy",""),
     ]),
-    ("FLAT","CLARIAH-PLUS WP3 T062 & T063; CLARIAH-CORE WP3 T24 [D2.10 (software); D2.11 (docs)]", [
-        ("proycon", "flat"),
-        ("proycon", "foliadocserve"),
+    ("FLAT","CLARIAH-PLUS WP3 T062","https://github.com/LanguageMachines/clariah-plus-tasks/raw/master/di.huc/FLAT.T062.pdf", [
+        ("proycon", "flat", "T062D1"),
+        ("proycon", "foliadocserve", "T062D2"),
     ]),
-    ("LaMachine","CLARIAH-PLUS WP3 T098; CLARIAH CORE WP3: LaMachine v2 plan in scope of CLARIAH WP3 VRE", [
-        ("proycon", "LaMachine"),
-        ("proycon", "labirinto"),
-        ("proycon", "codemetapy"),
+    ("LaMachine","CLARIAH-PLUS WP3 T098", "https://github.com/LanguageMachines/clariah-plus-tasks/raw/master/di.huc/LaMachine.T098.pdf", [
+        ("proycon", "LaMachine", "T098D1"),
+        ("proycon", "labirinto","T098D2"),
+        ("fbkarsdorp", "lamachine-homebrew","T098D3"),
+        ("proycon", "codemetapy","T098D4"),
+        ("proycon", "lamastats","T098D6"),
     ]),
-    ("Ucto","CLARIAH-CORE WP3 T55 [D1.1 (software), D1.2 (docs)]", [
-        ("LanguageMachines", "ucto"),
-        ("LanguageMachines", "uctodata"),
-        ("proycon", "python-ucto"),
+    ("Frog, Ucto & DeepFrog","CLARIAH-PLUS WP3 T139", "https://github.com/LanguageMachines/clariah-plus-tasks/raw/master/di.huc/Frog.T139.pdf",  [
+        ("LanguageMachines", "frog","T139aD1"),
+        ("LanguageMachines", "frogdata","T139aD1.2"),
+        ("proycon", "python-frog","T139aD1.3"),
+        ("LanguageMachines", "toad",""),
+        ("LanguageMachines", "ucto","T139aD2"),
+        ("LanguageMachines", "uctodata","T139aD2.2"),
+        ("proycon", "python-ucto","T139aD2.3"),
+        ("proycon", "deepfrog","T139bD1"),
     ]),
-    ("Frog","CLARIAH-PLUS WP3 T139; CLARIAH-CORE WP3 T22 [D2.1 (software); D2.2 (doc)]; T23 [D2.3 (froggen software), D2.4 (froggen docs)]", [
-        ("LanguageMachines", "frog"),
-        ("LanguageMachines", "frogdata"),
-        ("proycon", "python-frog"),
-        ("LanguageMachines", "toad"),
+    ("PICCL & TICCL","CLARIAH-PLUS WP3 ???","", [
+        ("LanguageMachines", "PICCL",""),
+        ("LanguageMachines", "ticcltools",""),
     ]),
-    ("PICCL & TICCL","CLARIAH-PLUS WP3 ???; CLARIAH-CORE WP3 T26 [D1.3 (PICCL software); T1.4 (PICCL docs)]", [
-        ("LanguageMachines", "PICCL"),
-        ("LanguageMachines", "ticcltools"),
+    ("Nederlab","Nederlab","", [
+        ("proycon", "nederlab-pipeline",""),
+        ("proycon", "colibri-utils",""),
     ]),
-    ("Nederlab","Nederlab", [
-        ("proycon", "nederlab-pipeline"),
-        ("proycon", "colibri-utils"),
+    ("Timbl & Mbt","Pre-CLARIN","", [
+        ("LanguageMachines", "timbl",""),
+        ("LanguageMachines", "timblserver",""),
+        ("LanguageMachines", "mbt",""),
+        ("LanguageMachines", "mbtserver",""),
+        ("proycon", "python-timbl",""),
     ]),
-    ("Timbl & Mbt","Pre-CLARIN", [
-        ("LanguageMachines", "timbl"),
-        ("LanguageMachines", "timblserver"),
-        ("LanguageMachines", "mbt"),
-        ("LanguageMachines", "mbtserver"),
-        ("proycon", "python-timbl"),
+    ("Miscellaneous","Dependencies/wrappers and or unforeseen tools (related to CLARIAH projects)","", [
+        ("LanguageMachines", "ticcutils",""),
+        ("proycon", "aNtiLoPe",""),
+        ("proycon", "wikiente",""),
     ]),
-    ("Miscellaneous","Dependencies/wrappers and or unforeseen tools (related to CLARIAH projects)", [
-        ("LanguageMachines", "ticcutils"),
-        ("proycon", "aNtiLoPe"),
-        ("proycon", "wikiente"),
-        ("proycon", "piereling"),
-    ]),
-    ("Software Quality Guidelines","CLARIAH-CORE WP2 Task 54.100", [
-        ("CLARIAH", "software-quality-guidelines"),
+    ("Software Quality Guidelines","CLARIAH-CORE WP2 Task 54.100","", [
+        ("CLARIAH", "software-quality-guidelines",""),
     ]),
 ]
 
 if args.all:
     sources += [
-        ("Gecco & Valkuil", "Implicit Linguisitics, Revisely", [
-            ("proycon","gecco"),
-            ("proycon","valkuil-gecco"),
-            ("proycon","valkuil"),
+        ("Gecco & Valkuil", "Implicit Linguisitics, Revisely","", [
+            ("proycon","gecco",""),
+            ("proycon","valkuil-gecco",""),
+            ("proycon","valkuil",""),
         ]),
-        ("Spreek2Schrijf", "Tweede Kamer der Staten Generaal", [
-           ("proycon", "spreek2schrijf"),
+        ("Spreek2Schrijf", "Tweede Kamer der Staten Generaal","", [
+           ("proycon", "spreek2schrijf",""),
         ]),
-        ("Colibri", "Colibri PhD project", [
-           ("proycon", "colibri-core"),
-           ("proycon", "colibri-mt"),
-           ("proycon", "colibrita"),
+        ("Colibri", "Colibri PhD project","", [
+           ("proycon", "colibri-core",""),
+           ("proycon", "colibri-mt",""),
+           ("proycon", "colibrita",""),
         ]),
-        ("Colloquery", "Van Dale", [
-            ("proycon","colloquery"),
+        ("Colloquery", "Van Dale","", [
+            ("proycon","colloquery",""),
         ]),
-        ("T-Scan", "Utrecht University", [
-            ("proycon","tscan"),
+        ("T-Scan", "Utrecht University","", [
+            ("proycon","tscan",""),
         ]),
-        ("Oersetter", "Fryske Akademy", [
-            ("proycon","oersetter-webservice"),
-            ("proycon","oersetter-models"),
+        ("Oersetter", "Fryske Akademy","", [
+            ("proycon","oersetter-webservice",""),
+            ("proycon","oersetter-models",""),
         ]),
-        ("BabelEnte", "TraMOOC", [
-            ("proycon","babelente"),
-            ("proycon","babelpy"),
+        ("BabelEnte", "TraMOOC","", [
+            ("proycon","babelente",""),
+            ("proycon","babelpy",""),
         ]),
-        ("Deprecated projects", "(unfunded)", [
-            ("LanguageMachines", "LuigiNLP"),
+        ("Deprecated projects", "(unfunded)","", [
+            ("LanguageMachines", "LuigiNLP",""),
         ]),
-        ("Shared-task software", "(unfunded)", [
-            ("LanguageMachines" ,"CLIN28_ST_spelling_correction"),
-            ("proycon" ,"anavec"),
+        ("Shared-task software", "(unfunded)","", [
+            ("LanguageMachines" ,"CLIN28_ST_spelling_correction",""),
+            ("proycon" ,"anavec",""),
         ]),
-        ("Various support software", "(mostly unfunded)", [
-            ("proycon", "lamastats"),
-            ("proycon", "parseme-support"),
-            ("proycon", "beeldbankvertaler"),
+        ("Various support software", "(mostly unfunded)","", [
+            ("proycon", "parseme-support",""),
+            ("proycon", "beeldbankvertaler",""),
         ]),
     ]
 
@@ -129,8 +128,8 @@ headers = {
 }
 
 users = set()
-for _,_, repos in sorted(sources):
-    for user, repo in repos:
+for _,_,_,repos in sorted(sources):
+    for user, repo,_ in repos:
         users.add(user)
 
 #Get a list of all repositories for the users
@@ -175,18 +174,23 @@ print("(TODO: add manually)")
 print()
 
 
-for group, references, repos in sorted(sources):
+for group, references, tasklink, repos in sorted(sources):
     print(f"## {group}")
     print()
-    print("*Project/task/deliverable references:* ", references)
+    if tasklink:
+        print(f"*Project & Task ID:* [{references}]({tasklink})")
+    else:
+        print("*Project & Task ID:* ", references)
     print()
     found = False
     if args.verbose:
         print("*Repository details*:")
         print()
-        for user, repo in sorted(repos, key= lambda x: x[1].lower()):
+        for user, repo, del_id in sorted(repos, key= lambda x: x[1].lower()):
             found = False
             print("* **" + str(user_repos[user][repo.lower()]['name']) + f"** - ``https://github.com/{user}/{repo}``")
+            if del_id:
+                print("  - *deliverable ID*: " + str(del_id))
             print("  - *description*: " + str(user_repos[user][repo.lower()]['description']))
             if user_repos[user][repo.lower()]['homepage']:
                 print("  - *website*: " + str(user_repos[user][repo.lower()]['homepage']))
@@ -199,7 +203,7 @@ for group, references, repos in sorted(sources):
         print()
         print("*Releases:*")
         print()
-    for user, repo in sorted(repos, key= lambda x: x[1].lower()):
+    for user, repo, del_id in sorted(repos, key= lambda x: x[1].lower()):
         first = True
         url = f"https://api.github.com/repos/{user}/{repo}/releases"
         while first or ('next' in response.links and 'url' in response.links['next']):
@@ -219,14 +223,16 @@ for group, references, repos in sorted(sources):
                             namelabel = names[release['author']['login']]
                         except KeyError:
                             namelabel = release['author']['login']
-                        print(f"*(Released by " + namelabel + " on " + release['published_at'][:10] + ")*\n"  + f"https://github.com/{user}/{repo}/releases/tag/" + release['tag_name'])
+                        print(f"*([Released](https://github.com/{user}/{repo}/releases/tag/" + release['tag_name'] + ") by " + namelabel + " on " + release['published_at'][:10] + ")*\n"  + f"https://github.com/{user}/{repo}/releases/tag/" + release['tag_name'])
+                        if del_id:
+                            print(f"*(deliverable ID: {del_id})*")
                         print()
             if not found and args.verbose:
-                print(f"*(no releases for " + repo + " this period)*")
+                print("*(no releases for " + repo + " this period)*")
                 print()
             if 'next' in response.links and 'url' in response.links['next']:
                 url = response.links['next']['url']
     if not found and not args.verbose:
-        print(f"*(no releases this period)*")
+        print("*(no releases this period)*")
         print()
     print()
